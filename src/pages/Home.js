356cb9337e9def13f5bcff4dbb989f87461c0f4b
Kaustubh Ladiya <k4ustu3h@gmail.com>
2023-04-20 02:46:29 +0530

    Redesign Hero Section on Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
+import FilledInput from "@mui/material/FilledInput";
 import FoodItems from "../components/FoodItems";
 import Footer from "../components/Footer";
+import FormControl from "@mui/material/FormControl";
 import Grid from "@mui/material/Unstable_Grid2";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import InputLabel from "@mui/material/InputLabel";
 import Navbar from "../components/NavBar";
+import Typography from "@mui/material/Typography";
 import { ThemeProvider } from "@mui/material";
 import { themeOptions } from "../styles/themeOptions";
+import { Icon } from "@iconify/react";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function Home() {
 	const [foodCat, setFoodCat] = useState([]);
@@ -33,98 +43,101 @@ export default function Home() {
 	return (
 		<ThemeProvider theme={themeOptions}>
 			<CssBaseline />
-			<div>
-				<Navbar />
-			</div>
-			<div>
-				<div
-					id="carouselExampleFade"
-					className="carousel slide carousel-fade "
-					data-bs-ride="carousel"
+			<Navbar />
+			<Box
+				sx={{
+					width: "100%",
+					minHeight: "100vh",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					padding: { xs: 2, md: 24 },
+				}}
+			>
+				<Grid
+					container
+					spacing={6}
+					sx={{
+						display: "flex",
+						alignItems: { xs: "flex-start", md: "center" },
+						flexDirection: { xs: "column-reverse", md: "row" },
+						maxWidth: { xs: "100%", md: 1280 },
+					}}
 				>
-					<div className="carousel-inner " id="carousel">
-						<div className=" carousel-caption  " style={{ zIndex: "9" }}>
-							<div className=" d-flex justify-content-center">
-								{" "}
-								{/* justify-content-center, copy this <form> from navbar for search box */}
-								<input
-									className="form-control me-2 w-75 bg-white text-dark"
+					<Grid item xs={12} md={6} sx={{ pb: { xs: 2, md: 42 } }}>
+						<Typography
+							variant="h3"
+							fontWeight={700}
+							pb={6}
+							maxWidth={512}
+							color="primary.main"
+						>
+							Hungry? You're in the right place
+						</Typography>
+						<Typography variant="h6" pb={4} maxWidth={720}>
+							Be it healthy or junk food, we offer a wide range of cuisines to
+							satisfy your taste buds.
+						</Typography>
+						<Button
+							variant="contained"
+							color="primary"
+							href="#menu"
+							sx={{ width: { xs: "100%", md: 200 } }}
+						>
+							Take Me to the Menu
+						</Button>
+						<Box sx={{ mt: 8 }}>
+							<FormControl sx={{ width: "100%" }}>
+								<InputLabel htmlFor="search-box" sx={{ ml: 4, mt: 0.5 }}>
+									Search
+								</InputLabel>
+								<FilledInput
+									id="search-box"
 									type="search"
-									placeholder="Search in here..."
+									disableUnderline
 									aria-label="Search"
 									value={search}
 									onChange={(e) => {
 										setSearch(e.target.value);
 									}}
+									label="Search"
+									sx={{ pl: 2, pb: 1, borderRadius: 8 }}
+									endAdornment={
+										<InputAdornment
+											position="end"
+											sx={{
+												visibility: search ? "visible" : "hidden",
+												mt: 1,
+												mr: 1,
+											}}
+										>
+											<IconButton
+												onClick={() => {
+													setSearch("");
+												}}
+												edge="end"
+											>
+												<Icon icon="ic:outline-close" width="24" />
+											</IconButton>
+										</InputAdornment>
+									}
 								/>
-								<button
-									className="btn text-white bg-danger"
-									onClick={() => {
-										setSearch("");
-									}}
-								>
-									X
-								</button>
-							</div>
-						</div>
-						<div className="carousel-item active">
-							<img
-								src="https://source.unsplash.com/random/900x700/?burger"
-								className="d-block w-100  "
-								style={{ filter: "brightness(30%)" }}
-								alt="..."
-							/>
-						</div>
-						<div className="carousel-item">
-							<img
-								src="https://source.unsplash.com/random/900x700/?pastry"
-								className="d-block w-100 "
-								style={{ filter: "brightness(30%)" }}
-								alt="..."
-							/>
-						</div>
-						<div className="carousel-item">
-							<img
-								src="https://source.unsplash.com/random/900x700/?barbeque"
-								className="d-block w-100 "
-								style={{ filter: "brightness(30%)" }}
-								alt="..."
-							/>
-						</div>
-					</div>
-					<button
-						className="carousel-control-prev"
-						type="button"
-						data-bs-target="#carouselExampleFade"
-						data-bs-slide="prev"
-					>
-						<span
-							className="carousel-control-prev-icon"
-							aria-hidden="true"
-						></span>
-						<span className="visually-hidden">Previous</span>
-					</button>
-					<button
-						className="carousel-control-next"
-						type="button"
-						data-bs-target="#carouselExampleFade"
-						data-bs-slide="next"
-					>
-						<span
-							className="carousel-control-next-icon"
-							aria-hidden="true"
-						></span>
-						<span className="visually-hidden">Next</span>
-					</button>
-				</div>
-			</div>
-			<div className="container">
-				{" "}
-				{/* boootstrap is mobile first */}
+							</FormControl>
+						</Box>
+					</Grid>
+					<Grid item xs={12} md={6} sx={{ mt: { xs: 8, md: 20 } }}>
+						<Player
+							autoplay
+							loop
+							src="https://assets6.lottiefiles.com/packages/lf20_tll0j4bb.json"
+						></Player>
+					</Grid>
+				</Grid>
+			</Box>
+			<div className="container" id="menu">
 				{foodCat !== []
 					? foodCat.map((data) => {
 							return (
-								// justify-content-center
 								<div className="row mb-3">
 									<div key={data._id} className="fs-3 m-3">
 										{data.CategoryName}

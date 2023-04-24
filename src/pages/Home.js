b9ab8893d9d48adece89e224aa7fab1d2e2c7783
Kaustubh Ladiya <k4ustu3h@gmail.com>
2023-04-24 21:45:01 +0530

    Add Skeletons to Home while Food Items are not loaded

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import FoodItems from "../components/FoodItems";
 import Footer from "../components/Footer";
 import Grid from "@mui/material/Unstable_Grid2";
 import Navbar from "../components/NavBar";
+import Skeleton from "@mui/material/Skeleton";
 import Typography from "@mui/material/Typography";
 import { ThemeProvider } from "@mui/material";
 import HeroSection from "../components/HeroSection";
@@ -57,52 +58,78 @@ export default function Home() {
 				searchVisibility={search ? "visible" : "hidden"}
 			/>
 			<Container id="menu">
-				{foodCat !== []
-					? foodCat.map((data) => {
-							return (
-								<Box key={data._id} mb={3}>
-									<Typography gutterBottom variant="h4">
-										{data.CategoryName}
-									</Typography>
-									<Divider
-										sx={{
-											my: 2,
-										}}
+				{foodCat.length === 0 ? (
+					<Box mb={3}>
+						<Typography gutterBottom variant="h4">
+							<Skeleton variant="text" width="50%" />
+						</Typography>
+						<Divider sx={{ my: 2 }} />
+						<Grid container spacing={4}>
+							{Array.from(Array(8)).map((_, index) => (
+								<Grid key={index} xs={12} sm={6} md={4} lg={3}>
+									<Skeleton
+										variant="rectangular"
+										width="100%"
+										sx={{ height: 176, borderRadius: 4 }}
 									/>
-									<Grid container spacing={4}>
-										{foodItems.length > 0 &&
-											foodItems
-												.filter(
-													(items) =>
-														items.CategoryName === data.CategoryName &&
-														items.name
-															.toLowerCase()
-															.includes(search.toLowerCase())
-												)
-												.map((filterItems) => {
-													return (
-														<Grid
-															key={filterItems._id}
-															xs={12}
-															sm={6}
-															md={4}
-															lg={3}
-														>
-															<FoodItems
-																foodName={filterItems.name}
-																description={filterItems.description}
-																item={filterItems}
-																options={filterItems.options[0]}
-																ImgSrc={filterItems.img}
-															></FoodItems>
-														</Grid>
-													);
-												})}
-									</Grid>
-								</Box>
-							);
-					  })
-					: ""}
+									<Skeleton variant="text" width="80%" sx={{ mt: 1 }} />
+									<Skeleton variant="text" width="60%" />
+									<Box display="flex" mt={1}>
+										<Skeleton
+											variant="circular"
+											sx={{ height: 40, width: 40 }}
+										/>
+										<Skeleton
+											variant="circular"
+											sx={{ height: 40, width: 40, ml: "auto" }}
+										/>
+									</Box>
+								</Grid>
+							))}
+						</Grid>
+					</Box>
+				) : (
+					foodCat.map((data) => {
+						return (
+							<Box key={data._id} mb={3}>
+								<Typography gutterBottom variant="h4">
+									{data.CategoryName}
+								</Typography>
+								<Divider sx={{ my: 2 }} />
+								<Grid container spacing={4}>
+									{foodItems.length > 0 &&
+										foodItems
+											.filter(
+												(items) =>
+													items.CategoryName === data.CategoryName &&
+													items.name
+														.toLowerCase()
+														.includes(search.toLowerCase())
+											)
+											.map((filterItems) => {
+												return (
+													<Grid
+														key={filterItems._id}
+														xs={12}
+														sm={6}
+														md={4}
+														lg={3}
+													>
+														<FoodItems
+															foodName={filterItems.name}
+															description={filterItems.description}
+															item={filterItems}
+															options={filterItems.options[0]}
+															ImgSrc={filterItems.img}
+														></FoodItems>
+													</Grid>
+												);
+											})}
+								</Grid>
+							</Box>
+						);
+					})
+				)}
 			</Container>
 			<Footer onChange={handleModeChange}></Footer>
 		</ThemeProvider>
